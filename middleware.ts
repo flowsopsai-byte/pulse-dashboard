8f828c8d573565703f1f348b06dcc7bf4890b9c3
Thomas <flowsopsai@gmail.com>
2026-08-16 16:00:47 +0700

    fix: exclude manifest and icons from PIN middleware

--- middleware.ts
+++ middleware.ts
@@ -16,5 +16,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next|favicon.ico|api).*)']
+  matcher: ['/((?!_next|favicon.ico|api|manifest.json|icon-).*)']
 }
